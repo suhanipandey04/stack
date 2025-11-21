9beb1f4ca4ad157398328e53142e880043336ac5
suhanipandey04 <dheeverse@gmail.com>
2025-11-21 19:59:05 +0530

    5

---
+++ stock_span.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    //stock prices
+    vector<int> price = {100,80,60,70,60,75,85};
+
+    //solution
+    vector<int> ans(price.size(),0);
+    stack<int> s;
+
+    for(int i=0;i<price.size();i++)
+    {
+        while(s.size() > 0 && price[s.top()] <= price[i])
+        {
+            s.pop();
+        }
+        if(s.empty())
+        {
+            ans[i] = i+1;
+        }
+        else{
+            ans[i]=i-s.top(); //i-prevhigh
+        }
+        s.push(i);
+    }
+
+    //print ans vals
+    for(int val : ans)
+    {
+        cout << val << " ";
+    }
+    cout<<endl;
+
+    return 0;
+}
